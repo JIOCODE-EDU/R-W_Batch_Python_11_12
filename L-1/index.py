d0a3f86bfd8754c144bb4c6aad1941dde67500d7
red and white skill <ifmdac80@gmail.com>
2025-06-06 12:27:20 +0530

    06-06-2025

---
+++ L-1/index.py
@@ -0,0 +1,23 @@
+#Program 1
+#print Hello World Program
+
+print("Hello World!")
+
+
+#Program 2
+# print your name
+
+name = input("Enter Your Name :")
+print('Hello' , name)
+
+#Program 3
+# check positive , nagative or zero
+
+num = int(input("Enter your number:"))
+
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Nagative")
+else:
+    print("Zero")
