43deef236f0134cd590bb4590d3c472349b8e2cd
red and white skill <ifmdac80@gmail.com>
2025-06-09 12:07:04 +0530

    09-06-2025

--- L-6/loop.py
+++ L-6/loop.py
@@ -21,15 +21,17 @@
       # i+=1 [ i = 2 + 1 ] = 3
       # i+=1 [ i = 3 + 1 ] = 4
       # i+=1 [ i = 4 + 1 ] = 5
-# i = 0 
-# while i <= 10:
-#   print("while loop:" , i)
-#   i+=1
+i = 0 
+while i <= 10:
+  print("while loop:" , i)
+  i+=1
 
 # Nested Loop 
 
-for i in range(1 , 5):
-  for j in range(1 , 4):
-    print(j)
-  print()
+# for i in range(1 , 5):
+#   for j in range(1 , 4):
+#     print(j)
+#   print()
+
+
 
