5dcecb42865cc582dcf2ad25d9808163273fafb4
red and white skill <ifmdac80@gmail.com>
2025-06-13 12:27:15 +0530

    13-06-2025

---
+++ Revision/13-06.py
@@ -0,0 +1,24 @@
+# While Loop 
+
+# while condition(true / false):
+#   statement run
+
+# age = 0
+
+dicision = True
+
+while dicision:
+  pro1 = input("Enter your number : ")
+  if(pro1 == "1"):
+      print("Pro1")
+  elif(pro1 == "2"):
+      print("pro2")
+  elif(pro1 == "3"):
+      print("you have exit this program!!!")
+      dicision = False
+      print(dicision)
+  else:
+    print("choose right number")
+
+  # while True:
+  #   pro2 = input("Enter your number")
