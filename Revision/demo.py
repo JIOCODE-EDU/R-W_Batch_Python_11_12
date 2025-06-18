7cfd3f18d6bede778ab86582db86dbb793b277eb
red and white skill <ifmdac80@gmail.com>
2025-06-18 12:44:38 +0530

    18-06-2025

---
+++ Revision/demo.py
@@ -0,0 +1,64 @@
+# Python Basics
+
+print("Hello World!!")
+
+
+# User Input
+
+# input()
+
+# Variables
+
+x = 10
+
+y = "python program"
+
+box = "toys"
+
+isOpen = True
+
+print(x)
+
+print(y)
+
+print(box)
+
+print(isOpen)
+
+# DataTypes
+# type()
+
+print(type(x))
+
+print(type(y))
+
+print(type(box))
+
+print(type(isOpen))
+
+# Python Operation
+
+
+a = 20
+b = 30
+c = 50
+
+print(a+b+c)
+
+# User Input
+
+name = input("Enter Your Name : ")
+
+print(name)
+
+print(type(name))
+
+# DataType Conversation
+
+age = int(input("Enter Your Age : "))
+
+print(age)
+
+print(type(age))
+
+
