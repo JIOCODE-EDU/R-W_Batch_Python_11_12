855e85feafe9b57392380dd33c080ef25a1d4584
red and white skill <ifmdac80@gmail.com>
2025-06-21 12:19:03 +0530

    21-06-2025

---
+++ Revision/revision-21.py
@@ -0,0 +1,42 @@
+# Python Loop
+
+# print("Hello World!")
+# print("Hello World!")
+# print("Hello World!")
+# print("Hello World!")
+# print("Hello World!")
+# print("Hello World!")
+# print("Hello World!")
+# print("Hello World!")
+# print("Hello World!")
+# print("Hello World!")
+
+# for i in range(0 , 10):
+#   print("Hello World!!")
+
+
+# Conditional Statement in Python
+
+# age = 20
+
+# if age >= 18:
+#   print("you are aligible for voting!")
+# else:
+#   print("you are not aligible for voting")
+
+# ladder if else statements in python
+
+# subject = "biology"
+
+# if subject == "chemistry":
+#   print("chemistry book vailable!")
+# elif subject == "pysics":
+#   print("pysics book vailable!")
+# elif subject == "maths":
+#   print("maths book vailable!")
+# elif subject == "english":
+#   print("english book vailable!")
+# elif subject == "history":
+#   print("history book vailable!")
+# else:
+#   print("book unvailable!")
