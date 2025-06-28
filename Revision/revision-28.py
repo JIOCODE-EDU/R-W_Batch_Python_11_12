8594b0a12dc7e82fa49151699ba4e052e4314c05
red and white skill <ifmdac80@gmail.com>
2025-06-28 12:32:08 +0530

    28-06-2025

---
+++ Revision/revision-28.py
@@ -0,0 +1,110 @@
+# What Is List / Array in Python [ Mutable ]
+
+# student_1 = "Ankit"
+# student_2 = "vishal"
+# student_3 = "meet"
+# student_4 = "maheswari"
+# student_5 = "radhika"
+
+# print(student_1)
+# print(student_2)
+# print(student_3)
+# print(student_4)
+# print(student_5)
+
+# student_name_list = ["Ankit" , "vishal" , "meet" , "maheswari" , "radhika"]
+
+# for val in student_name_list:
+#   print(val)
+
+my_list = [1 , 2 , 3 , 4 , 5 , 6]
+
+# my_list.append(6)
+
+# print(my_list)
+
+# list index
+# list length
+
+# print(my_list[2])
+# print(len(my_list))
+
+# my_list[0] = 10 # modifying list
+# my_list[4] = 50 # modifying list
+
+# print(my_list)
+
+# print(my_list)
+
+# my_list.append(10)
+
+# print(my_list)
+
+# del my_list[0]
+
+# print(my_list)
+
+
+# Tuple [ Immutable ]
+# you cannot midify element after creation.
+
+my_tuple = (1 , 2 , 3 , 4 , 5 , 6)
+
+# my_tuple[0] = 10
+
+# del my_tuple[0]
+
+# my_tuple.append(10)
+
+# print(my_tuple)
+
+# list = tuple(my_list)
+
+# print(list)
+
+
+# set [ Mutable ]
+
+# you can modify element after creation.
+
+my_set = {1 , 2 , 3 , 4 , 5 , 6}
+
+# print(my_set)
+
+# my_set.add(5)
+# my_set.add(8)
+
+# # del my_set[1]
+
+# print(my_set)
+
+# Dictionary [ Mutable ]
+
+# Stores key-value pairs.
+
+# my_dict = {"key1":1 , "key2":2 , "key3":3  , "key4":4}
+
+# print(my_dict)
+
+# my_dict["key1"] = 10
+
+# del my_dict["key2"]
+
+# print(my_dict)
+
+# Type Casting Constructure
+
+# a = [1 , 2 , 3]
+
+# b = tuple(a)
+
+# c = set(b)
+
+# d = dict([('x' , 1) , ('y' , 2)])
+
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+
